a0f6d9f2515b3548fd4f6be7cadba0e5b42c312a
Vincent Buit <vincent@vincentbuit.nl>
2022-10-06 21:58:48 +0200

    Add fake data generation option for now

--- init_db.sh
+++ init_db.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # set -eux
 
-init_db () {
-    DB="test.db"
-    cat ./migrations/221006_init_db.sql | sqlite3 $DB
-}
-
-init_db
+DB="test.db"
+cat ./migrations/221006_init_db.sql | sqlite3 $DB
+case "$1" in
+test)
+    echo "generating fake data"
+    cat ./migrations/_fake_data.sql | sqlite3 $DB
+    ;;
+*)
+    echo "no arguments were given so no fake data is generated"
+    ;;
+esac
---
+++ migrations/_fake_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO tag (name) VALUES 
+    ( "Vegetarisch" ),
+    ( "Italiaans" );
+
+
+INSERT INTO ingredient (name, unit) VALUES 
+    ( "Rijst", "gram" ),
+    ( "Ei", null ),
+    ( "Bloem", "gram" ),
+    ( "Tomaten", null );
+
+INSERT INTO recipe (name, category, prep_time, cook_time, servings, intro, description, rating, source) VALUES 
+    ( "Pannenkoeken", 3, 5, 30, 1, "intro", "description", 5, "source" ),
+    ( "Pasta", 2, 15, 20, 1, "intro", "description", 3.5, "source" );
+
+INSERT INTO tag_mapping VALUES 
+    ( 1, 1 ),
+    ( 2, 2 );
+
+INSERT INTO recipe_ingredient VALUES 
+    ( 1, 2, 4, 1 ),
+    ( 1, 3, 500, 1 ),
+    ( 2, 1, 500, 1 ),
+    ( 2, 4, 8, 1 );
